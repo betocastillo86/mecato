fe6bf62f6b3a6b0b07af952027bfe636a3270dd9
Gabriel <gabriel.castillo86@hotmail.com>
2015-12-16 18:18:48 -0500

    Creación del sitio

---
+++ readme.md
@@ -0,0 +1,1 @@
+Plugin de wordpress para bicimensajería
---
+++ src/wp-content/plugins/mecato/mecato.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Mecato
+ * Author: Gabriel Castillo
+ * Version: 0.0.1
+ * Description: Plugin para encontrar restaurante vegetariano
+ */
+
+
+if (defined('URE_PLUGIN_URL')) {
+    wp_die('It seems that other version of User Role Editor is active. Please deactivate it before use this version');
+}
+
+
+
